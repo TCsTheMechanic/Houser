8defcc48f3e9733726e899ae706555b5e7cc36ae
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-04-10 20:16:44 -0300

    Change to import at main file

--- app/services/speechCreator.py
+++ app/services/speechCreator.py
@@ -1,9 +1,10 @@
 from gtts import gTTS
 import os
 
-# Cria o mp3 com o texto indicado
-tts = gTTS(text="I'm fine, but I think you should make me think", lang="en")
-tts.save("fineThink.mp3")
+def voice(text):
+  # Cria o mp3 com o texto indicado
+  tts = gTTS(text=text, lang="en")
+  tts.save("voice.mp3")
 
-# Da o comando para o cmd executar o mp3
-os.system("fineThink.mp3")
+  # Da o comando para o cmd executar o mp3
+  os.system("cd assets/audios & voice.mp3")
