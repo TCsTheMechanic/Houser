dffff9faf547c41f4cc3e9c6a8bb821a36d51e64
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-04-10 20:16:07 -0300

    Change to receive data from mongo

--- app/services/textToSpeachTest.py
+++ app/services/textToSpeachTest.py
@@ -1,27 +1,21 @@
 import os
 import speech_recognition as sr
+from speechCreator import voice
+
 houser = sr.Recognizer()
 
 speech = ""
 
 while(speech != "goodbye Houser"):
-    with sr.Microphone() as source:
-        print("----------Start----------")
-        audio = houser.listen(source)
-        print("----recording is over----")
+  with sr.Microphone() as source:
+    print("----------Start----------")
+    audio = houser.listen(source)
+    print("----recording is over----")
 
-    try:
-        speech = "" + houser.recognize_google(audio)
-        print("Person said: " + speech)
-        if(speech == "hello Houser"):
-            os.system("cd assets/audios & hello.mp3")
-        elif(speech == "good night") or (speech == "good night Houser"):
-            os.system("cd assets/audios & goodNight.mp3")
-        elif(speech == "goodbye Houser"):
-            os.system("cd assets/audios & bye.mp3")
-        elif (speech == "how are you"):
-            os.system("cd assets/audios & fineThink.mp3")
-        else:
-            os.system("cd assets/audios & repeatPlz.mp3")
-    except:
-        pass
+  try:
+    speech = "" + houser.recognize_google(audio)
+    print("Person said: " + speech)
+    if(speech == "data from mongo"):
+      voice(speech)
+  except:
+    pass
