7a4097b966a020c2c5cd1cb49e1f1d980adeb600
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-04-12 13:32:22 -0300

    Finish the database infor search

--- app/services/recorder.py
+++ app/services/recorder.py
@@ -1,24 +1,25 @@
 import os
 import speech_recognition as sr
-from speechCreator import voice
-from db.connection import connect
 
-database = connect()
-dialog_collection = database.Dialog
+def listener(connect, voice):
+  database = connect
+  dialog_collection = database.Dialog
 
-houser = sr.Recognizer()
-speech = ''
+  houser = sr.Recognizer()
+  speech = ''
 
-while(speech != 'goodbye Houser'):
-  with sr.Microphone() as source:
-    print('----------Start----------')
-    audio = houser.listen(source)
-    print('----recording is over----')
+  while(speech != 'goodbye Houser'):
+    with sr.Microphone() as source:
+      print('----------Start----------')
+      audio = houser.listen(source)
+      print('----recording is over----')
 
-  try:
-    speech = '' + houser.recognize_google(audio)
-    print('Person said: ' + speech)
-    if(speech == 'ask from mongo'):
-      voice('answer from mongo')
-  except:
-    pass
+    try:
+      speech = '' + houser.recognize_google(audio)
+      print('Person said: ' + speech)
+      for dialog in dialog_collection:
+        print(dialog)
+        if(speech == dialog['question']):
+          voice(dialog['answer'])
+    except:
+      pass
