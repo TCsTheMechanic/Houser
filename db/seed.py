c02d996490c410e5d08808bd6f3c72f80b246df3
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-04-14 21:57:10 -0300

    Refactor seed.py

--- db/seed.py
+++ db/seed.py
@@ -1,22 +1,16 @@
-from connection import connect
+def dialog_seeder(connect):
 
-db = connect()
-
-collection = db.Dialog
-
-documents = [
-  {
-    'question': 'hey houser',
-    'answer': 'how can I help you?'
-  },
-  {
-    'question': 'can you turn on the light?',
-    'answer': 'yes, please tell me the room'
-  },
-  {
-    'question': 'goodbye Houser',
-    'answer': 'goodbye'
-  }
-]
-
-collection.insert_many(documents)
+  connect.Dialog.insert_many([
+    {
+      'question': 'hey houser',
+      'answer': 'how can I help you?'
+    },
+    {
+      'question': 'can you turn on the light?',
+      'answer': 'yes, please tell me the room'
+    },
+    {
+      'question': 'goodbye Houser',
+      'answer': 'goodbye'
+    }
+  ])
