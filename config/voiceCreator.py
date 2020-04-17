9b855430b7d15c4aee5c6624fc516d2c545ab7d2
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-04-17 15:53:57 -0300

    Check if the .mp3 file does't exists

--- config/voiceCreator.py
+++ config/voiceCreator.py
@@ -1,9 +1,11 @@
 from gtts import gTTS
+import os
 
 def voice(connect):
   dialog_collection = connect.Dialog
 
   for dialog in dialog_collection.find():
-    tts = gTTS(text=dialog['answer'], lang='en')
-    # I chose to name the audio by question bc question never have special char
-    tts.save('app/assets/audios/' + dialog['question'] + '.mp3')
+    if (os.path.isfile('app/assets/audios/' + dialog['question'] + '.mp3') == False):
+      tts = gTTS(text=dialog['answer'], lang='en')
+      # I chose to name the audio by question bc question never have special char
+      tts.save('app/assets/audios/' + dialog['question'] + '.mp3')
