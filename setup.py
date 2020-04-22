ebe640e16bd47fe475542a133af292b62fb2e262
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-04-22 18:50:08 -0300

    Add audios folder creation

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from config.voiceCreator import voice
 from db.seed import dialog_seeder
+import os 
+
+if (os.path.isdir('app/assets') == False):
+  os.makedirs('app/assets/audios')
 
 dialog_seeder()
 voice()
