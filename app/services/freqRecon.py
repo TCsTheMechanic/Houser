83073cb3924483106accc170e0ce75c676f09d44
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-05-06 18:04:39 -0300

    Extrac voice frequency to hz

--- app/services/freqRecon.py
+++ app/services/freqRecon.py
@@ -3,8 +3,21 @@ import struct
 import numpy as np
 import matplotlib.pyplot as matplot
 
-CHUNK = 1024 * 4
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
+CHUNK = 2048
 
+houser = pyaudio.PyAudio()
+
+stream = houser.open(
+  format = pyaudio.paInt16,
+  channels = 1,
+  rate = 44100,
+  input = True,
+  output = True,
+  frames_per_buffer = CHUNK
+)
+
+while True:
+  data = stream.read(CHUNK)
+  data_integer = struct.unpack(str(2 * CHUNK) + 'B', data)
+  data_array = np.array(data_integer, dtype = 'b')[::2]
+  print(data_array)
