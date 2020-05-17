9af4242239cf337450a043cfa90e0ffb4522c256
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-05-17 17:45:00 -0300

    Add many commands

--- db/seed.py
+++ db/seed.py
@@ -20,11 +20,39 @@ def dialog_seeder():
     },
     {
       'command': 'add another person',
-      'answer': 'alright, please, say hey houser three times'
+      'answer': 'alright, please, when I ask you will say hey houser three times'
     },
     {
       'command': 'who are you',
       'answer': "Hello, my name is Houser and I'm Eduardo Cerutti's final project, nice to meet you"
+    },
+    {
+      'command': 'ask frequency 1',
+      'answer': 'say the first time please'
+    },
+    {
+      'command': 'ask frequency 2',
+      'answer': 'the second'
+    },
+    {
+      'command': 'ask frequency 3',
+      'answer': 'and the last one'
+    },
+    {
+      'command': 'thank you',
+      'answer': "you're welcome"
+    },
+    {
+      'command': 'command nice to meet you',
+      'answer': 'nice to meet you'
+    },
+    {
+      'command': 'command ask person name',
+      'answer': "what's the person's name?"
+    },
+    {
+      'command': 'command thanks',
+      'answer': 'thank you'
     },
     {
       'command': 'listening error',
