5eee0037efd1b8de7925e2391c8bd9f9d17ffd46
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-05-17 18:40:15 -0300

    Fix no reply issue

--- app/services/recorder.py
+++ app/services/recorder.py
@@ -13,28 +13,31 @@ def start():
   speech = ''
 
   while(speech != 'goodbye Houser'):
-    try:
-      speech = listener(houser)
-      print('Person said: ' + speech)
-      understood = False
-      for dialog in dialog_collection.find():
-        if (speech == dialog['command']):
-          understood = dialog['command']
-      if (understood != False):
-        if (understood == 'add another person'):
-          os.system('cd app/assets/audios & "command as person name.mp3"')
-          person_name = listener(houser)
-          catcher(person_name)
-        else:
-          os.system('cd app/assets/audios & "' + understood + '.mp3"')
+    speech = listener(houser)
+    understood = False
+
+    for dialog in dialog_collection.find():
+      if (speech == dialog['command']):
+        understood = dialog['command']
+
+    if (understood != False):
+      if (understood == 'add another person'):
+        os.system('cd app/assets/audios & "command as person name.mp3"')
+        person_name = listener(houser)
+        catcher(person_name)
       else:
-        os.system('cd app/assets/audios & "listening error.mp3"')
-    except:
-      pass
+        os.system('cd app/assets/audios & "' + understood + '.mp3"')
+    else:
+      os.system('cd app/assets/audios & "listening error.mp3"')
 
 def listener(houser):
-  with sr.Microphone() as source:
-    print('----------Start----------')
-    audio = houser.listen(source)
-    print('----recording is over----')
-    return '' + houser.recognize_google(audio)
+  try:
+    with sr.Microphone() as source:
+      print('----------Start----------')
+      audio = houser.listen(source)
+      print('----recording is over----')
+    speech = '' + houser.recognize_google(audio)
+    print('Person said: ' + speech)
+    return speech
+  except:
+      pass
