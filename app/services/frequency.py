d1dc607ac7e648f48bd6c6e0458890f9a06449c4
Eduardo Cerutti de Oliveira <ceruttioliveira@hotmail.com>
2020-05-17 17:45:50 -0300

    Adjust frequency.py to interact with recorder.py

--- app/services/frequency.py
+++ app/services/frequency.py
@@ -1,11 +1,15 @@
+from db.connection import connect
 import numpy as np
 import pyaudio
 import struct
+import os
 
 def __init__():
   pass
 
-def catcher(user_name, database):
+def catcher(user_name):
+  person_collection = connect().Person
+
   CHUNK = 2048
 
   houser = pyaudio.PyAudio()
@@ -19,15 +23,20 @@ def catcher(user_name, database):
     frames_per_buffer = CHUNK
   )
 
+  os.system('cd app/assets/audios & "add another person.mp3"')
+
   for index in range(3):
+    os.system('cd app/assets/audios & "ask frequency ' + index + '.mp3"')
     data_array = []
     data = stream.read(CHUNK)
     data_integer = struct.unpack(str(2 * CHUNK) + 'B', data)
     data_array << np.array(data_integer, dtype = 'b')[::2]
-    database.Person.inser_one(
+    person_collection.inser_one(
       {
         'name': user_name,
         'sample': index,
         'frequency': data_array
       }
     )
+
+  os.system('cd app/assets/audios & "command nice to meet you.mp3"'')
