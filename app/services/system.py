8975041699cbd4ef764da0f40a791e9ba962906a
TCsTheMechanic <ceruttioliveira@hotmai.com>
2020-06-01 18:49:46 -0300

    Create function listen_frequency and change listen function name to listen_speech

--- app/services/system.py
+++ app/services/system.py
@@ -1,10 +1,15 @@
+from db.connection import connect
 import speech_recognition as sr
+import numpy as np
+import pyaudio
+import struct
+import time
 import os
 
 def __init__():
   pass
 
-def listen():
+def listen_speech():
   houser = sr.Recognizer()
 
   try:
@@ -18,7 +23,34 @@ def listen():
     return speech
 
   except:
-      pass
+    pass
+
+def listen_frequency(timeout):
+  CHUNK = 2048
+
+  houser = pyaudio.PyAudio()
+
+  stream = houser.open(
+    format = pyaudio.paInt16,
+    channels = 1,
+    rate = 44100,
+    input = True,
+    output = True,
+    frames_per_buffer = CHUNK
+  )
+
+  record_timeout = time.time() + timeout
+  data_list = []
+
+  print('----------Start----------')
+
+  while (time.time() < record_timeout):
+    data = stream.read(CHUNK)
+    data_list.extend(struct.unpack(str(2 * CHUNK) + 'B', data))
+
+  print('----recording is over----')
+
+  return data_list
 
 def reply(audio):
   os.system('cd app/assets/audios & "' + audio + '.mp3"')
